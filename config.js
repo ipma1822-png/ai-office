62bba87ebed5c59215e510899e6d028bc11bf85c
IPMA Global <ipma.1822@gmail.com>
2026-09-08 12:52:13 +0900

    connect GN24 publish handoff module

--- config.js
+++ config.js
@@ -13,7 +13,8 @@ window.AI_OFFICE_CONFIG = Object.freeze({
 (()=>{
   const modules=[
     './gen-news-v350.js?v=3.5.0',
-    './gen-newsroom-v350.js?v=3.5.0'
+    './gen-newsroom-v350.js?v=3.5.0',
+    './gn24-publish-bridge-v350.js?v=3.5.0'
   ];
   modules.forEach(src=>{
     const script=document.createElement('script');
